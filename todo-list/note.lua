06d9ba252986b9f1fe3cece8e1bff085f0eff739
jojo <yoseph@pop-os.gateway>
2025-02-14 12:20:12 +0800

    Adjusted notes.lua and added NotesManager.lua
    NotesManager which manages the notes for application

--- todo-list/note.lua
+++ todo-list/note.lua
@@ -1,6 +1,5 @@
 -- Defining the notes object attributes
 Note = {
-	id = "",
 	title = "",
 	description = "",
 }
@@ -22,18 +21,10 @@ function Note:getDescription(s)
 	return self.description;
 end
 
-function Note:setId(s)
-	self.id = s;
-end
-
-function Note:getId(s)
-	self.id = s;
-end
-
 -- Define the constructor
 function Note:new(t)
-	t = t or {}
-	setmetatable(t, self)
-	self.__index = self
-	return t
+	t = t or {};
+	setmetatable(t, self);
+	self.__index = self;
+	return t;
 end
